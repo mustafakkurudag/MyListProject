dfb508d6e836d3232908a4f06ad2065207ecc0be
mustafakkurudag <mustafakurudag@gmail.com>
2022-09-14 23:35:47 +0300

    Project completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,27 +12,55 @@ public class Main {
         list.add(1);
         list.add("Merhaba");
         list.add(2);
-        System.out.println("Listenin 20. elemanı: " + list.get(20));
-        list.add("Merhaba2");
+        list.add("Bu bir");
         list.add(3);
-        list.add("Merhaba3");
+        list.add("liste projesi");
         list.add(4);
+        list.add(2);
 
-
-
-        System.out.println(list.getoArray());
+        System.out.println(list);
         System.out.println("Listedeki eleman sayısı: " + list.size());
         System.out.println("Listenin kapasitesi: " + list.getCapacity());
 
-        list.add(5);
-        list.add(6);
-        list.add("Hello");
-        list.add(7);
-        list.add(9);
-        list.add(234234);
+        //girilen parametredeki indisteki elemanı siler
+        list.remove(0);
+        System.out.println(list);
 
-        System.out.println(list.getoArray());
+        list.set(0, "Hello");
+        System.out.println(list);
         System.out.println("Listedeki eleman sayısı: " + list.size());
         System.out.println("Listenin kapasitesi: " + list.getCapacity());
+        System.out.println("1. indisteki eleman: " + list.get(1));
+        //girilen parametrenin bulunduğu ilk indis
+        System.out.println(list.indexOf(2));
+        //girilen parametrenin bulunduğu son indis
+        System.out.println(list.lastIndexOf(2));
+
+        //listenin boş ise true dolu ise false döndürür.
+        System.out.println(list.isEmpty());
+        MyList<String> myStrList = new MyList<>();
+        System.out.println(myStrList.isEmpty());
+
+        //toArray() metodu liste elemanlarını diziye çevirir.
+        Object[] objArr = list.toArray();
+        for (Object o :
+                objArr) {
+            System.out.println(o);
+        }
+
+        //listedeki elemanları temizler.
+        //list.clear();
+        System.out.println(list);
+        System.out.println("Listedeki eleman sayısı: " + list.size());
+        System.out.println("Listenin kapasitesi: " + list.getCapacity());
+
+        //listenin ilgili parametreleri aralığında bir alt liste döndürür.
+        MyList<Object> subMylist = list.subList(2,5);
+        System.out.println("2. ve 5. indis aralığındaki alt liste: ");
+        System.out.println(subMylist);
+        //parametredeki eleman listede varsa true yoksa false döndürür
+        System.out.println(list.contains(4));
+        System.out.println(list.contains(40));
+
     }
 }
--- src/com/company/MyList.java
+++ src/com/company/MyList.java
@@ -51,13 +51,37 @@ public class MyList<T> {
     public T get(int index) {
         for (int i = 0; i < this.oArray.length; i++) {
             if (i == index) {
-                return (T) this.oArray[i-1];
+                return (T) this.oArray[i];
             }
         }
         return null;
     }
 
-    public String getoArray() {
+    public void remove(int index) {
+        if (index < 0 || index > this.size()) {
+            System.out.println("Yanlış indis değeri");
+        } else {
+            for (int i = 0; i <=index; i++) {
+                if (i == index) {
+                    for (int j = index; j < this.oArray.length - 1; j++ ) {
+                        this.oArray[j] = this.oArray[j + 1];
+                    }
+                }
+            }
+        }
+    }
+
+    public void set(int index, T data) {
+        if (get(index) != null) {
+            for (int i = 0; i < this.size(); i++) {
+                if (i == index) {
+                    this.oArray[i] = data;
+                }
+            }
+        }
+    }
+
+    public String toString() {
         String startStr = "[";
         String endStr = "]";
         StringBuilder centralStr = new StringBuilder();
@@ -69,6 +93,77 @@ public class MyList<T> {
                 centralStr.append(this.oArray[i]).append(", ");
         }
         return startStr + centralStr + endStr;
+    }
+
+    public int indexOf(T data) {
+        for (int i = 0; i < size(); i++) {
+            if (this.oArray[i] == data) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(T data) {
+        int count = 0;
+        for (int i = 0; i < size(); i++) {
+            if (this.oArray[i] == data) {
+                count++;
+            }
+        }
+        if (count == 0)
+            return -1;
+        else if (count == 1) {
+            return indexOf(data);
+        } else {
+            for (int i = size() - 1; i >= 0; i--) {
+                if (this.oArray[i] == data) {
+                    return i;
+                }
+            }
+        }
+        return count;
+    }
+
+    public boolean isEmpty() {
+        if (size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public <T> T[] toArray() {
+        T[] tArray = (T[]) new Object[size()];
+        for (int i = 0; i < size(); i++) {
+            tArray[i] = (T) this.oArray[i];
+        }
+        return tArray;
+    }
+
+    public void clear() {
+        System.out.println(size());
+        for (int i = 0; i < this.oArray.length; i++) {
+            this.oArray[i] = null;
+        }
+    }
+
+    public MyList<T> subList(int start, int finish) {
+        MyList<T> sub = new MyList<>();
+        for (int i = start; i < finish; i++) {
+            sub.add((T) this.oArray[i]);
+        }
+
+        return sub;
+    }
+
+    public boolean contains(T data) {
+        for (Object o : this.oArray) {
+            if (o == data) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public int getCapacity() {
